daab50ca4a1eb8c19dadd28ee1b5e7ccc4dfd5c1
henriquedevb <henriquedevb@gmail.com>
2021-06-17 15:46:10 -0300

    primeiro commit

---
+++ Database/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+class Connection {
+    constructor() {
+      this.mongodb();
+    }
+    mongodb() {
+      mongoose.connect(
+          (process.env.DB_HOST) ? `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority` 
+          : "mongodb://localhost/pacientes", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true,
+      }).then(() => {
+        console.log("Conexão DB realizada com sucesso!");
+      }).catch((exception) => {
+        console.log("Erro de conexão com o banco." + exception);
+      });
+    }
+  }
+  
+module.exports =  new Connection();
